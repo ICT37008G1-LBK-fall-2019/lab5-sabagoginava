536570ea761c2896c315220d2270f9a90fa933c8
sabagoginava <sabagoginava2@gmail.com>
2019-11-03 15:13:59 +0400

    laab5

---
+++ L502/script.js
@@ -0,0 +1,5 @@
+function empty(x){
+   return x.children.length == 0; // abrunebs trues
+}
+let ul = document.getElementById('empty-list')
+console.log(empty(ul))
---
+++ L504/script.js
@@ -0,0 +1,6 @@
+var mainblock = document.querySelector('#phone-container');
+let phoneList = mainblock.getElementsByClassName('phone-item');
+for (let phone of phoneList) {
+   let text = phone.querySelector('.model').textContent;
+   console.log(text)
+}
